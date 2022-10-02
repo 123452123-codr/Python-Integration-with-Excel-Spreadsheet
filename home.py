81d32e1cdc07758260e691f32e7182f0c023bac2
123452123-codr <81916900+123452123-codr@users.noreply.github.com>
2022-10-02 11:19:12 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,2 +1,11 @@
+import imp
 from openpyxl.workbook import Workbook
 from openpyxl import load_workbook
+
+wb = load_workbook('database.xlsx')
+ws = wb.active
+
+ws["A1"].value = 'Hello'
+
+wb.save('database.xlsx')
+
