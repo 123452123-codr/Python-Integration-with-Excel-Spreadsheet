7e9ddcb8197e67f847def4c25dd31c4f8301f81c
123452123-codr <81916900+123452123-codr@users.noreply.github.com>
2022-10-05 20:54:57 +0530

    Edits to include input strings

--- home.py
+++ home.py
@@ -1,9 +1,13 @@
+from random import randint, random
 from openpyxl import Workbook, load_workbook
 
 wb = load_workbook('database.xlsx')
 ws = wb.active
 
-ws['A1'].value = "Pi"
+a = input("Enter a string to input into the database : ")
 
+ws["A1"].value = a
+
+print("String successfully inserted. Returned with code {}".format(randint(2312, 38217)))
 
 wb.save('database.xlsx')
